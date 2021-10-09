b7b9f0447010e22ac6f1247ef36a3391fca32f5c
msj0319 <gaeddongie13@gmail.com>
2021-10-10 03:40:37 +0900

    Add Consumer Log

--- src/main/java/bts/auction/api/consumer/controller/AuctionController.java
+++ src/main/java/bts/auction/api/consumer/controller/AuctionController.java
@@ -5,6 +5,7 @@ import bts.auction.api.consumer.service.AuctionService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
-
+@Slf4j
 @Api(value = "Auction Consumer Controller")
 @RequiredArgsConstructor
 @RestController
@@ -22,7 +23,7 @@ public class AuctionController {
     @ApiOperation(value = "모든 NFT의 경매 매수 참여 정보 조회")
     @GetMapping("auction/{nftid}")
     public Mono<Auction> consumedMessage(@PathVariable String nftid) {
-
+        log.info("[Consumer - AuctionController] {"+nftid+"}의 최고 희망매수입찰가 반환");
         return auctionService.findByNftId(nftid);
     }
 
